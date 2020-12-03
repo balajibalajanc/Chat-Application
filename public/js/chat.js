90d36ed101e6a6f456cd727ee7b1bf1fc90be69b
balajibalajanc <balajibalajanci@gmail.com>
2020-12-03 16:44:31 +0530

    Added location sharing functionalities and acknowledgement functionalities

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,5 +11,29 @@ const weatherform=document.querySelector('#message-form');
  weatherform.addEventListener('submit',(e)=>{
      e.preventDefault();
      const message=e.target.elements.message
-      socket.emit('sendMessage',message.value);
+      socket.emit('sendMessage',message.value,(error)=>{
+          if(error)
+          {
+              return console.log(error);
+          }
+          console.log("Message delivered");
+      });
+ })
+
+ document.querySelector('#send-location').addEventListener('click',()=>{
+     if(!navigator.geolocation)
+     {
+         return alert('Sorry geolocation is not supported');
+     }
+     navigator.geolocation.getCurrentPosition((position)=>{
+         console.log(position);
+         
+         socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        },()=>{
+            console.log("Location Shared");
+        });
+     })
+
  })
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port=process.env.PORT || 3000
 const app=express()
 const server=http.createServer(app)
 const io=socketio(server);
-
+const Filter=require('bad-words');
 
 app.use(express.static(public_dir_path))
 
@@ -21,9 +21,19 @@ io.on('connection',(socket)=>{
     //console.log("New connection to Moodboard Chat Application");
     socket.emit('connectionMade','Welcome to the MoodBoard Chat Application');
     socket.broadcast.emit('connectionMade','A new User hae been joined');
-        socket.on('sendMessage',(sentFromClient)=>{
+        socket.on('sendMessage',(sentFromClient,callback)=>{
+            const filter= new Filter();
+            if(filter.isProfane(sentFromClient))
+            {
+                return callback("profanity is not allowed")
+            }
              io.emit('connectionMade',sentFromClient);
-  
+            callback();
+         })
+
+         socket.on('sendLocation',(coords,callback)=>{
+            io.emit('connectionMade',`https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`);
+            callback();
          })
 
          socket.on('disconnect',()=>{
